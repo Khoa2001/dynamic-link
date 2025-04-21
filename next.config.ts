1c3f710f6ce653d14ee15a79ef61dba356b7d478
Mai Dang Khoa <mdkkhoa1002@gmail.com>
2025-04-21 11:17:51 +0700

    Update

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,7 @@
 import { NextConfig } from 'next';
 import createNextIntlPlugin from 'next-intl/plugin';
 
-const nextConfig: NextConfig = {
-  ignoreDuringBuilds: true,
-};
+const nextConfig: NextConfig = {};
 
 const withNextIntl = createNextIntlPlugin();
 export default withNextIntl(nextConfig);
